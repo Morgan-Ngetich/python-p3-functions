f0f45a50483e1e100fee10596baed198f57573cd
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-12-05 22:15:08 +0300

    All test passed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Guido")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum = add(45, 55)
+print(sum)
 
 def halve(number):
-    pass
+#<isinstance> Used to verify if an object belongs to a particular type
+   if not isinstance(number, (int, float)):
+       return None
+   return number / 2
+
+result = halve(4)
+  
